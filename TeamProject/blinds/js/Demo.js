e2c7ab817c16a2b7ce93b5049252dfd73e5ae785
lixinyi <244lixinyi@163.com>
2016-03-14 23:31:44 +0800

    task 01

--- TeamProject/blinds/js/Demo.js
+++ TeamProject/blinds/js/Demo.js
@@ -2,35 +2,49 @@
  * Created by Tane on 2016/3/12.
  */
     //版本一
-/*var h3Arr = document.getElementsByTagName("h3");
-var mainDom = document.getElementById("main");
-var liArr = mainDom.querySelectorAll("ul");
-for(var i=0;i<h3Arr.length;i++){
-    h3Arr[i].onclick=function(i){
-        if(this.nextElementSibling.hasAttribute("class")){
-            this.nextElementSibling.removeAttribute("class");//block
-        }else{
-            this.nextElementSibling.setAttribute("class","active");
+//在数组中剔除本身并不可行，必须在循环中剔除，所以就会造成数组长度变短。
+Array.prototype.remove = function (dx) {
+    for (var i = 0, n = 0; i < this.length; i++) {
+        if (this[i] != this[dx]) {
+            this[n++] = this[i];
         }
     }
-}*/
-//版本二
+    this.length -= 1;
+}
+
 var h3Arr = document.getElementsByTagName("h3");
 var mainDom = document.getElementById("main");
 var liArr = mainDom.querySelectorAll("ul");
-console.log(h3Arr)
-console.log(mainDom)
-console.log(liArr)
-for(var i in h3Arr){
-//    h3Arr[i].addEventListener('click',function(){
-//        alert(i)
-//    },true)
-    h3Arr[i].onclick=function(){
-        if(this.nextElementSibling.hasAttribute("class")){
-            this.nextElementSibling.removeAttribute("class");//block
-        }else{
-            this.nextElementSibling.setAttribute("class","active");
+var arr = [];
+for (var i = 0; i < h3Arr.length; i++) {
+    h3Arr[i].index = i;
+    arr.push(h3Arr[i].index);
+    h3Arr[i].onclick = function () {
+        if (h3Arr[this.index].nextElementSibling.hasAttribute("class")) {//block
+            h3Arr[this.index].nextElementSibling.removeAttribute("class");
+        } else {
+            h3Arr[this.index].nextElementSibling.setAttribute("class", "active");
         }
     }
 }
+
+////版本二
+//var h3Arr = document.getElementsByTagName("h3");
+//var mainDom = document.getElementById("main");
+//var liArr = mainDom.querySelectorAll("ul");
+//console.log(h3Arr)
+//console.log(mainDom)
+//console.log(liArr)
+//for(var i in h3Arr){
+////    h3Arr[i].addEventListener('click',function(){
+////        alert(i)
+////    },true)
+//    h3Arr[i].onclick=function(){
+//        if(this.nextElementSibling.hasAttribute("class")){
+//            this.nextElementSibling.removeAttribute("class");//block
+//        }else{
+//            this.nextElementSibling.setAttribute("class","active");
+//        }
+//    }
+//}
 //弄不出来了，GG好么。。补充知识去。。
